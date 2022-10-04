85f038f7f9975db1f6a86f88c330286f804139ef
Oliaoliaol <o_safina@mail.ru>
2022-10-04 21:45:54 +0300

    задачи на уроке

--- Program.cs
+++ Program.cs
@@ -1,2 +1,56 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿//Напишите программу, которая принимает двузначное число и показывает наибольшую цифру числа.
+ 
+/* int  FindBiggerDigit(int num)
+{
+    int result;
+
+    if (num < 10 || num>=100)
+        return 0;
+    else
+    {
+        int ed = num % 10;
+        int dec =num/10;
+
+        if(ed > dec)
+            result = dec;
+        else result =dec;
+            
+    }
+    return result;
+}
+
+Console.Write("Input a two digit number: ");
+int number = Convert.ToInt32(Console.ReadLine());
+
+int biggerDigit = FindBiggerDigit(number);
+Console.WriteLine($"Bigger digit of {number} is {biggerDigit}");
+*/
+
+//Напишите программу, которая выводит случайное трёхзначное число и удаляет вторую цифру этого числа.
+
+
+
+/*int DeleteSecondNumber(int num)
+{
+    int result;
+    int ed = num % 10;
+    int sot = num /100;
+
+    result = sot*10+ed;
+    return result;
+        
+}
+int number = new Random().Next(100, 999);
+
+int twoBigitNumber = DeleteSecondNumber(number);
+Console.WriteLine($"A three-digit number {number} without a second cipher {twoBigitNumber}");
+*/
+
+
+
+
+
+
+
+
+
